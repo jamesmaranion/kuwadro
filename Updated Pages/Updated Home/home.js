f2031cc86a7d8905a06e6ff19255efafcff573f0
JemsIzDed <90274036+jamesmaranion@users.noreply.github.com>
2021-10-27 13:00:13 +0800

    Add files via upload
    Updated Home Page

---
+++ Updated Pages/Updated Home/home.js
@@ -0,0 +1,33 @@
+const arrows = document.querySelectorAll(".arrow");
+const artList = document.querySelectorAll(".art-list");
+
+arrows.forEach((arrow, i) => {
+    const itemNo = artList[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrow.addEventListener("click", ()=>{
+        const ratio = Math.floor(window.innerWidth/270);
+        clickCounter++;
+        if (itemNo - (4 + clickCounter) + (4 - ratio) >= 0) {
+            artList[i].style.transform = `translateX(${
+              artList[i].computedStyleMap().get("transform")[0].x.value - 300
+            }px)`;
+        }else{
+            artList[i].style.transform="translateX(0)";
+            clickCounter = 0;
+        }
+    });
+});
+
+//toggle
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+    ".container,.art-list-title,.navbar-container,.logo,.toggle");
+
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+    ball.classList.toggle("active")
+})
+
