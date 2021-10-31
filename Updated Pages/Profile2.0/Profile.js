d49a2472175d4b25210bc3498faece71d18386f9
George Encarnacione <georgeangelo.encarnacion@benilde.edu.ph>
2021-10-31 11:38:20 +0800

    Updated Profile and css names

---
+++ Updated Pages/Profile2.0/Profile.js
@@ -0,0 +1,14 @@
+
+//toggle
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+    ".container,.navbar-container,.second-navbar-list,.navbar-container,.logo,.toggle");
+
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+    ball.classList.toggle("active")
+})
+
